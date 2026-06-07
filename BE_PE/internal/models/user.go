8c8188f28abe13de0e381811b4ca37ecc74d8efc
B1ZON-c0de <abdreyko.roma@gmail.com>
2026-06-07 21:09:07 +0700

    feat: добавил userRequest струкртуру

--- BE_PE/internal/models/user.go
+++ BE_PE/internal/models/user.go
@@ -9,3 +9,9 @@ type User struct {
 	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 }
+
+type UserRequestRegister struct {
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"email,omitempty"`
+	Password string `json:"password,omitempty"`
+}
