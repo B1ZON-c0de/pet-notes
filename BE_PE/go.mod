32a0bf9d840fb29fddb77872bc8bb68eb594f048
B1ZON-c0de <abdreyko.roma@gmail.com>
2026-06-07 18:41:44 +0700

    feat: доавбил метод регистрации пользователя

--- BE_PE/go.mod
+++ BE_PE/go.mod
@@ -2,4 +2,8 @@ module github.com/B1ZON-c0de/backend
 
 go 1.26.3
 
-require github.com/lib/pq v1.12.3 // indirect
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+	golang.org/x/crypto v0.52.0 // indirect
+)
---
+++ BE_PE/internal/service/auth.go
@@ -0,0 +1,106 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/B1ZON-c0de/backend/internal/models"
+	"github.com/B1ZON-c0de/backend/internal/repository"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	lenPassword = 6
+)
+
+var (
+	ErrEmailExists = errors.New("пользователь с такми email уже существует")
+	ErrEmailEmpty  = errors.New("поле email не может быть пустое")
+	ErrNameEmpty   = errors.New("поле name не может быть пустое")
+	ErrPassEmpty   = errors.New("поле password не может быть пустое")
+	ErrLenPassword = errors.New("поле password  должно содеожать более 6 символов")
+)
+
+type AuthService interface {
+	Register(ctx context.Context, name, email, password string) (string, error)
+	Login(ctx context.Context, email, password string) (string, error)
+	ValidateToken(ctx context.Context, token string) (string, error)
+}
+
+type authService struct {
+	repo      repository.UserRepo
+	jwtSecret string
+	jwtExpire time.Duration
+}
+
+func NewAuthService(repo repository.UserRepo, jwtSecret string, jwtExpire time.Duration) AuthService {
+	return &authService{
+		repo:      repo,
+		jwtSecret: jwtSecret,
+		jwtExpire: jwtExpire,
+	}
+}
+
+func (as *authService) Register(ctx context.Context, name, email, password string) (string, error) {
+	// Валидация кредов
+	if email == "" {
+		return "", ErrEmailEmpty
+	}
+	if name == "" {
+		return "", ErrNameEmpty
+	}
+	if password == "" {
+		return "", ErrPassEmpty
+	}
+	if len(password) < lenPassword {
+		return "", ErrLenPassword
+	}
+
+	// Хэширование пароля
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	user := models.User{
+		Name:     name,
+		Email:    email,
+		Password: string(hashedPassword),
+	}
+
+	//Создаем пользователя
+	if err := as.repo.Create(ctx, &user); err != nil {
+		if errors.Is(err, repository.ErrUserExists) {
+			return "", ErrEmailExists
+		}
+		return "", err
+	}
+
+	token, err := as.generateToken(user.ID)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func (as *authService) Login(ctx context.Context, email, password string) (string, error) {
+	return "", nil
+}
+
+func (as *authService) ValidateToken(ctx context.Context, token string) (string, error) {
+	return "", nil
+}
+
+func (as *authService) generateToken(userId string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userId,
+		"exp":     time.Now().Add(as.jwtExpire).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(as.jwtSecret))
+}
