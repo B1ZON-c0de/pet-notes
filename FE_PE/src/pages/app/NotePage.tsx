c109a39cba9491952904b5cc98cc584b3f5ddfc9
B1ZON-c0de <abdreyko.roma@gmail.com>
2026-06-10 01:33:22 +0700

    feat: добавил минимальный код для отображения записей

--- FE_PE/src/pages/app/NotePage.tsx
+++ FE_PE/src/pages/app/NotePage.tsx
@@ -1,5 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ROUTES_BACKEND } from "../../routesBackend";
+import { useParams } from "react-router";
+import type { Note } from "../../types";
+
 const NotePage = () => {
-  return <div>Страница записи</div>;
+  const { id } = useParams();
+  const [note, setNote] = useState<Note | null>(null);
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await axios.get(ROUTES_BACKEND.note(id));
+      setNote(res.data.data);
+    };
+    loadData();
+  }, [id]);
+  return (
+    <div>
+      {note && note.id} -{note && note.text}
+    </div>
+  );
 };
 
 export default NotePage;
--- FE_PE/src/pages/app/Notes.tsx
+++ FE_PE/src/pages/app/Notes.tsx
@@ -1,10 +1,25 @@
 import axios from "axios";
-import { Outlet, useLoaderData, useNavigate } from "react-router";
+import { Link, Outlet, useLoaderData, useNavigate } from "react-router";
 import { ROUTES_BACKEND } from "../../routesBackend";
-import { ROUTES } from "../../routes";
-import { AppShell, Button, Flex } from "@mantine/core";
+import { ROUTES, ROUTES_DYNAMICS } from "../../routes";
+import { AppShell, Button, Flex, TextInput } from "@mantine/core";
+import { useEffect, useState } from "react";
+import type { Note } from "../../types";
 
 const Notes = () => {
+  const [notes, setNotes] = useState<Note[] | null>(null);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await axios.get(ROUTES_BACKEND.notes, {
+        params: {
+          search: search ? search : undefined,
+        },
+      });
+      setNotes(res.data.data);
+    };
+    loadData();
+  }, [search]);
   const navigate = useNavigate();
   const { user } = useLoaderData();
   const logoutFn = async () => {
@@ -18,14 +33,26 @@ const Notes = () => {
       navbar={{ width: 300, breakpoint: "sm" }}
     >
       <AppShell.Header p="md">
-        <Flex>
+        <Flex justify="flex-end" gap="md">
+          <TextInput
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
           <p>{user.name}</p>
           <Button variant="filled" onClick={logoutFn} size="md">
             Выйти
           </Button>
         </Flex>
       </AppShell.Header>
-      <AppShell.Navbar p="md">Меню навигации</AppShell.Navbar>
+      <AppShell.Navbar p="md">
+        {notes &&
+          notes.map((note) => (
+            <Link to={ROUTES_DYNAMICS.note(note.id)} key={note.id}>
+              {note.title}
+              {note.text}
+            </Link>
+          ))}
+      </AppShell.Navbar>
       <AppShell.Main>
         <Outlet />
       </AppShell.Main>
