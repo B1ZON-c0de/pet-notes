a8394565b407a973b52f7e5ec1b9db9494e89fdb
B1ZON-c0de <abdreyko.roma@gmail.com>
2026-06-09 19:46:03 +0700

    feat: доабвил страцницу Логина

--- FE_PE/src/pages/auth/Login.tsx
+++ FE_PE/src/pages/auth/Login.tsx
@@ -1,5 +1,33 @@
+import { ROUTES } from "../../routes";
+import { BaseAuth } from "./BaseAuth";
+
+const descNav = {
+  textNav: "Ёще нет аккаунта?",
+  labelLink: "Регистрация",
+  path: ROUTES.register,
+};
+
+const loginFields = {
+  email: {
+    label: "Email",
+    initialValue: "",
+    placeholder: "user@mail.com",
+  },
+  password: {
+    label: "Пароль",
+    initialValue: "",
+  },
+};
+
 const Login = () => {
-  return <div>Логин</div>;
+  return (
+    <BaseAuth
+      pageLabel="Вход в NOTES"
+      submitLabel="Войти"
+      descNav={descNav}
+      fields={loginFields}
+    />
+  );
 };
 
 export default Login;
