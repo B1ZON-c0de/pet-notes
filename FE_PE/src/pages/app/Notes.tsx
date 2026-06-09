2ba53e639b27a4bd91c60af68791e107032c4af3
B1ZON-c0de <abdreyko.roma@gmail.com>
2026-06-10 00:56:20 +0700

    feat: добавил общий layout

--- FE_PE/src/pages/app/Notes.tsx
+++ FE_PE/src/pages/app/Notes.tsx
@@ -2,22 +2,34 @@ import axios from "axios";
 import { Outlet, useLoaderData, useNavigate } from "react-router";
 import { ROUTES_BACKEND } from "../../routesBackend";
 import { ROUTES } from "../../routes";
-import { Button } from "@mantine/core";
+import { AppShell, Button, Flex } from "@mantine/core";
 
 const Notes = () => {
   const navigate = useNavigate();
   const { user } = useLoaderData();
-  console.log(user);
   const logoutFn = async () => {
     await axios.post(ROUTES_BACKEND.logout);
     navigate(ROUTES.login, { replace: true });
   };
   return (
-    <div>
-      Приложение:
-      <Button onClick={logoutFn}> Выйти</Button>
-      <Outlet />
-    </div>
+    <AppShell
+      padding="md"
+      header={{ height: 80 }}
+      navbar={{ width: 300, breakpoint: "sm" }}
+    >
+      <AppShell.Header p="md">
+        <Flex>
+          <p>{user.name}</p>
+          <Button variant="filled" onClick={logoutFn} size="md">
+            Выйти
+          </Button>
+        </Flex>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">Меню навигации</AppShell.Navbar>
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
   );
 };
 
