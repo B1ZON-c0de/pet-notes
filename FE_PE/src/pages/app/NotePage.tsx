fe464490710d4d55f5a26c9c350f061493b9b56a
B1ZON-c0de <abdreyko.roma@gmail.com>
2026-06-10 21:20:38 +0700

    feat: доабвил кнопку редактровать

--- FE_PE/src/pages/app/NotePage.tsx
+++ FE_PE/src/pages/app/NotePage.tsx
@@ -1,17 +1,53 @@
 import { useLoaderData } from "react-router";
 import type { Note } from "../../types";
-import { Text } from "@mantine/core";
+import { Button, Flex, Text, Title } from "@mantine/core";
 import { dateFormatter } from "../../utils/dateFormatter";
+import Markdown from "react-markdown";
+import { useState } from "react";
 interface LoaderProps {
   note: Note;
 }
 const NotePage = () => {
   const { note } = useLoaderData<LoaderProps>();
+  const [isEditing, setIsEditing] = useState(false);
   return (
     <>
       <Text ta="center" c="dimmed">
         {dateFormatter(note.created_at, true)}
       </Text>
+      <Title order={1}>{note.title}</Title>
+      <Markdown>{note.text}</Markdown>
+      {isEditing ? (
+        <Flex pos="absolute" gap="md" align="center" bottom={20} right={20}>
+          <Button
+            variant="filled"
+            size="md"
+            color="gray"
+            onClick={() => setIsEditing(false)}
+          >
+            Отмена
+          </Button>
+          <Button
+            variant="filled"
+            color="green"
+            onClick={() => setIsEditing(false)}
+            size="md"
+          >
+            Сохранить
+          </Button>
+        </Flex>
+      ) : (
+        <Button
+          variant="filled"
+          pos="absolute"
+          bottom={20}
+          right={20}
+          size="md"
+          onClick={() => setIsEditing(true)}
+        >
+          Редактировать
+        </Button>
+      )}
     </>
   );
 };
