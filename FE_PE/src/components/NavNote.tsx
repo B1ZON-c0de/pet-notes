4990a23f2475947732cfe0ec5577cdd9cbb314a4
B1ZON-c0de <abdreyko.roma@gmail.com>
2026-06-10 15:28:44 +0700

    feat: добавил отображение навгации записей

--- FE_PE/src/components/NavNote.tsx
+++ FE_PE/src/components/NavNote.tsx
@@ -1,11 +1,38 @@
-import { Link } from "react-router";
+import { Link, useParams } from "react-router";
 import type { Note } from "../types";
 import { ROUTES_DYNAMICS } from "../routes";
+import { Flex, NavLink, Text, Title } from "@mantine/core";
+import { dateFormatter } from "../utils/dateFormatter";
+import { truncateText } from "../utils/truncateText";
 
 interface Props {
   note: Note;
 }
 
+function NoteTitle({ title }: { title: string }) {
+  return <Title order={5}>{title}</Title>;
+}
+
+function NoteDesc({ time, text }: { time: string; text: string }) {
+  return (
+    <Flex align="center" gap="md">
+      <Text fw={500} c="dark">
+        {dateFormatter(time)}
+      </Text>
+      <Text>{truncateText(text, 20)}</Text>
+    </Flex>
+  );
+}
+
 export default function NavNote({ note }: Props) {
-  return <Link to={ROUTES_DYNAMICS.note(note.id)}>{note.title}</Link>;
+  const { id } = useParams();
+  return (
+    <NavLink
+      component={Link}
+      active={note.id === id}
+      to={ROUTES_DYNAMICS.note(note.id)}
+      label={<NoteTitle title={note.title} />}
+      description={<NoteDesc time={note.updated_at} text={note.text} />}
+    />
+  );
 }
