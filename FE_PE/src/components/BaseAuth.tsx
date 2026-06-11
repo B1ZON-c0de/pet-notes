7df582083a422d49a0c5ccae11be3eab3fa7d8db
B1ZON-c0de <abdreyko.roma@gmail.com>
2026-06-11 16:04:53 +0700

    refactor: отрфекаторил BaseAuth вынес логику в хук useAuth

--- FE_PE/src/components/BaseAuth.tsx
+++ FE_PE/src/components/BaseAuth.tsx
@@ -8,18 +8,10 @@ import {
   TextInput,
   Title,
 } from "@mantine/core";
-import { useForm, type FormValidateInput } from "@mantine/form";
+import { useForm } from "@mantine/form";
 import { Link } from "react-router";
-import { useState } from "react";
-import axios from "axios";
-import { notifications } from "@mantine/notifications";
 import type { ROUTES } from "../routes";
-
-interface BaseField {
-  label: string;
-  initialValue: string;
-  placeholder?: string;
-}
+import { useAuth, type FormFields, type FormValues } from "../hooks/useAuth";
 
 interface DescNav {
   textNav: string;
@@ -27,32 +19,13 @@ interface DescNav {
   path: (typeof ROUTES)[keyof typeof ROUTES];
 }
 
-export interface FormValues {
-  name?: string;
-  email: string;
-  password: string;
-  repeatPassword?: string;
-}
-
 interface Props {
   pageLabel: string;
   submitLabel: string;
   descNav: DescNav;
   submitFn: (values: FormValues) => Promise<void>;
-  fields: {
-    name?: BaseField;
-    email: BaseField;
-    password: BaseField;
-    repeatPassword?: BaseField;
-  };
+  fields: FormFields;
 }
-
-const PASSWORD_LENGTH = 6;
-const NAME_LENGTH = 2;
-const ERR_INVALID_EMAIL = "Неверный email";
-const ERR_INVALID_NAME = `Длина имени должна быть минимум ${NAME_LENGTH} символа`;
-const ERR_INVALID_PASSWORD = `Длина пароля должна быть минимум ${PASSWORD_LENGTH} символов`;
-const ERR_INVALID_REPEAT_PASSWORD = "Пароли должны совпадать";
 
 export const BaseAuth = ({
   pageLabel,
@@ -61,30 +34,10 @@ export const BaseAuth = ({
   fields,
   submitFn,
 }: Props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const initialFieldsVal: FormValues = Object.entries(fields).reduce(
-    (acc, [key, field]) => {
-      if (!field) return acc;
-      acc[key as keyof FormValues] = field.initialValue;
-      return acc;
-    },
-    {} as FormValues,
-  );
-
-  const validateFields: FormValidateInput<FormValues> = {
-    password: (value: string) =>
-      value.length < 6 ? ERR_INVALID_PASSWORD : null,
-    email: (value: string) =>
-      /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) ? null : ERR_INVALID_EMAIL,
-  };
-  if (fields.repeatPassword) {
-    validateFields.repeatPassword = (value, values) =>
-      value !== values.password ? ERR_INVALID_REPEAT_PASSWORD : null;
-  }
-  if (fields.name) {
-    validateFields.name = (value) =>
-      value && value.length < 2 ? ERR_INVALID_NAME : null;
-  }
+  const { isLoading, initialFieldsVal, submitForm, validateFields } = useAuth({
+    submitFn,
+    fields,
+  });
 
   const form = useForm({
     mode: "uncontrolled",
@@ -96,27 +49,7 @@ export const BaseAuth = ({
     <>
       <Stack h="100vh" align="center" justify="center" gap="md">
         <Title order={1}>{pageLabel}</Title>
-        <form
-          onSubmit={form.onSubmit(async (values) => {
-            notifications.clean();
-            setIsLoading(true);
-            try {
-              await submitFn(values);
-              form.reset();
-            } catch (err) {
-              if (axios.isAxiosError(err)) {
-                notifications.show({
-                  title: "Ошибка",
-                  message: err.response?.data.error.message,
-                  color: "red",
-                  className: "notif-error",
-                });
-              }
-            } finally {
-              setIsLoading(false);
-            }
-          })}
-        >
+        <form onSubmit={form.onSubmit((values) => submitForm(values, form))}>
           <Fieldset variant="filled" w={350}>
             <Stack gap="md">
               {Object.entries(fields).map(([key, { label, placeholder }]) =>
